6b031f4808b3d1372d7a1a0416f12d6d33a0ac07
Olha Diakiv <olya.dyakiv26@gmail.com>
2026-03-30 13:14:53 +0200

    feat: initial commit

---
+++ src/ui/Input/index.tsx
@@ -0,0 +1,9 @@
+export function InputField() {
+    return (
+        <Fiel>
+            <FieldLabel htmlFor="input-field-username">Username</FieldLabel>
+            <Input id="input-field-username" type="text" placeholder="Enter your username" />
+            <FieldDescription>Choose a unique username for your account.</FieldDescription>
+        </Fiel>
+    )
+}
