a41e47b525e8620a6401ead286b6f4f8d5e59f37
Olha Diakiv <olya.dyakiv26@gmail.com>
2026-04-17 17:11:11 +0200

    feat: add delete functionality for checked and all products in ProductsList

--- src/features/products-list/ProductsList.tsx
+++ src/features/products-list/ProductsList.tsx
@@ -2,9 +2,12 @@ import AddNewProduct from './components/AddNewProduct'
 import Product from './components/Product'
 import type { Product as ProductType } from './types'
 import { useProducts } from './hooks/useProducts'
+import { Button } from '@/components/ui/button'
+import { useDeleteProduct } from './hooks/useDeleteProduct'
 
 export default function ProductsList() {
     const { products, isLoading } = useProducts()
+    const { deleteProduct, isCreating } = useDeleteProduct()
 
     const groupedProducts = products.reduce<Record<string, ProductType[]>>(
         (acc, product) => {
@@ -16,8 +19,25 @@ export default function ProductsList() {
         {} as Record<string, ProductType[]>,
     )
 
+    function handleDeleteAll() {
+        products.forEach(product => deleteProduct(product.id))
+    }
+
+    function handleDeleteChecked() {
+        const checked = products.filter(product => product.isChecked === true)
+        checked.forEach(product => deleteProduct(product.id))
+    }
+
     return (
         <div className="max-w-96">
+            <div className="flex gap-2 flex-wrap mb-4">
+                <Button disabled={isCreating} onClick={handleDeleteChecked}>
+                    Clear checked items
+                </Button>
+                <Button disabled={isCreating} onClick={handleDeleteAll}>
+                    Clear all
+                </Button>
+            </div>
             <AddNewProduct />
             {!isLoading
                 ? Object.entries(groupedProducts).map(([category, items]) => (
